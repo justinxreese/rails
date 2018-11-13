fbae256e27c8bc69b6c5bfaa1c10f1c941cbc0da
Justin Reese <justin.x.reese@gmail.com>
2018-11-13 18:30:43 -0500

    Adds foreign key discard

--- activestorage/app/jobs/active_storage/purge_job.rb
+++ activestorage/app/jobs/active_storage/purge_job.rb
@@ -2,7 +2,7 @@
 
 # Provides asynchronous purging of ActiveStorage::Blob records via ActiveStorage::Blob#purge_later.
 class ActiveStorage::PurgeJob < ActiveStorage::BaseJob
-  discard_on ActiveRecord::RecordNotFound
+  discard_on ActiveRecord::RecordNotFound, ActiveRecord::InvalidForeignKey
   retry_on ActiveRecord::Deadlocked, attempts: 10, wait: :exponentially_longer
 
   def perform(blob)
